caab71d74463d96f6d21149f0a36e487b7e5fec1
Ricardo Baumann <ricardo.baumann@zenvia.com.br>
2015-02-10 11:35:02 -0200

    missions loop

--- features/step_definitions/kf_steps.rb
+++ features/step_definitions/kf_steps.rb
@@ -9,3 +9,14 @@ Given /^loop on missions$/ do
 	  	}		
 	end
 end
+
+Given /^loop on fights$/ do
+	10.times do
+		steps %{
+			* click on "Missão" Text
+			* choose option "gesinnung" with "Cavaleiro das sombras em missão de saque"
+			* click on input with src like "img/lang/br/btn_weiter.jpg"
+			* Wait for "660" seconds
+	  	}		
+	end
+end
